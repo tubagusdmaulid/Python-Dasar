163ff5dd1e65fd0357123a3850d30bdcadbbc1c3
Tubagus Dinda Maulid <73454569+tubagusdmaulid@users.noreply.github.com>
2021-02-20 16:30:52 +0700

    Create 4_TipeDataNumber.py

---
+++ 4_TipeDataNumber.py
@@ -0,0 +1,7 @@
+# Tipe Data Number
+# Ada 4 type data number di python
+
+int_num = 10  # nilai int = bilangan bulat
+float_num = 10.2  # nilai float = bilangan desimal
+complex_num = 3.14j  # nilai complex
+long_num = 1234567L  # nilai Long
