eb438bdda20ec2b5adad6be5939061f95d3b6fef
Tubagus Dinda Maulid <73454569+tubagusdmaulid@users.noreply.github.com>
2021-02-20 15:56:52 +0700

    Update 2_Comment.py

--- 2_Comment.py
+++ 2_Comment.py
@@ -1,5 +1,11 @@
 # ini adalah sintaks komentar di Python
-# Hanya ada single comment
+# Tidak ada sintak khusus untuk multi comment
 # komentar tidak dapat di eksekusi oleh interpreter
 
 print("Hello World!") #Menampilkan Teks Hello World!
+
+#Alternatif Multi comment
+"""
+Ini adalah Alternatif Multi Comment
+Teks ini tidak akan di eksekusi
+"""
