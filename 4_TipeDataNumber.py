6840c70ac3d78c24649da01abd31698c3a34aa8b
Tubagus Dinda Maulid <73454569+tubagusdmaulid@users.noreply.github.com>
2021-02-20 16:59:10 +0700

    Update 4_TipeDataNumber.py

--- 4_TipeDataNumber.py
+++ 4_TipeDataNumber.py
@@ -1,7 +1,22 @@
 # Tipe Data Number
 # Ada 4 type data number di python
 
-int_num = 10  # nilai int = bilangan bulat
-float_num = 10.2  # nilai float = bilangan desimal
-complex_num = 3.14j  # nilai complex = bilangan real
-long_num = 1234567L  # nilai Long
+# nilai int = bilangan bulat
+bilanganPositif = 10
+bilanganNegatif = -10
+print(bilanganPositif)
+print(bilanganNegatif)
+
+# nilai float = bilangan desimal
+bilanganDesimalPositif = 10.2
+bilanganDesimalNegatif = -10.2
+print(bilanganDesimalPositif)
+print(bilanganDesimalNegatif)
+
+# nilai complex = ubah bilangan real menjadi bilangan complex
+bilanganComplex = 3.14j
+print(bilanganComplex)
+
+# nilai Long/Hexadesimal
+bilanganHexadesimal = 0x10
+print(bilanganHexadesimal)  # Output: 16
